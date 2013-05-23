ff7a8ae4e3b94681dad1045a48bc15de7fa30cfe
Martin Thomson <martin.thomson@gmail.com>
2013-05-22 20:30:25 -0700

    Fixing bugs, adding tests.

--- js/aweson.js
+++ js/aweson.js
@@ -1,178 +1,228 @@
 (function(g) {
-	'use strict';
+    'use strict';
 
-	var AWESON = g.AWESON = {};
+    var AWESON = g.AWESON = {};
 
-	var input, cursor;
+    var input, cursor;
 
-	function d() {}
-
-	function skipWhitespace() {
-		d('skipWhitespace', input.substring(cursor));
-		var ch = input.charAt(cursor);
-		var comment = ch === '"';
-		while (ch === ' ' || ch === '\t' || ch === '\r' || ch === '\n' || comment) {
-			++cursor;
-			ch = input.charAt(cursor);
-			if (comment) {
-				comment = ch !== '"';
-				if (!comment) {
-					++cursor;
-				}
-			}
-		}
-		return cursor;
-	}
-
-	function readName() {
-		d('readName', input.substring(cursor));
-		skipWhitespace();
-		var name = readValue();
-		if (input.charAt(cursor) !== '>') {
-			throw new Error('Missing > at ' + cursor);
-		}
+    var d;
+    if (process.env.DEBUG) {
+	d = function() { console.log.apply(console, arguments); };
+    } else {
+	d = function() { };
+    }
+    
+    function skipWhitespace() {
+	d('skipWhitespace', input.substring(cursor));
+	var ch = input.charAt(cursor);
+	var comment = ch === '"';
+	var quote;
+	while (ch === ' ' || ch === '\t' || ch === '\r' || ch === '\n' || comment) {
+	    ++cursor;
+	    ch = input.charAt(cursor);
+	    quote = ch === '"';
+	    if (quote) {
 		++cursor;
-		if (Array.isArray(name)) {
-			throw new Error('Whoa, that\'s not a good idea.');
-		}
+		ch = input.charAt(cursor);
+	    }
+	    comment ^= quote;
 	}
+	return cursor;
+    }
 
-	function readPair() {
-		d('readPair', input.substring(cursor));
-		skipWhitespace();
-		var pair = {};
-		switch (input.charAt(cursor)) {
-			case '>':
-				++cursor;
-				if (input.charAt(cursor) === '>') {
-					cursor++;
-					pair = null;
-				} else {
-					pair.value = readValue();
-				}
-				break;
-			case '<':
-				++cursor;
-				pair.name = readName(); // consumes '>'
-				pair.value = readValue();
-				break;
-			default:
-				throw new Error('I don\'t know what to make of character @' + cursor);
-		}
-		return pair;
+    function readName() {
+	d('readName', input.substring(cursor));
+	skipWhitespace();
+	var name = readString();
+	if (input.charAt(cursor) !== '>') {
+	    throw new Error('Missing > after name @' + cursor);
 	}
+	++cursor;
+	return name;
+    }
 
-	function notString(i) {
-		return typeof i.name !== 'string';
+    function readPair() {
+	d('readPair', input.substring(cursor));
+	skipWhitespace();
+	var pair = {};
+	switch (input.charAt(cursor)) {
+	case '>':
+	    ++cursor;
+	    if (input.charAt(cursor) === '>') {
+		cursor++;
+		pair = null;
+	    } else {
+		pair.value = readValue();
+	    }
+	    break;
+	case '<':
+	    ++cursor;
+	    pair.name = readName(); // consumes '>'
+	    pair.value = readValue();
+	    break;
+	default:
+	    throw new Error('I don\'t know what to make of character @' + cursor);
 	}
+	return pair;
+    }
 
-	function justTheValue(i) {
-		return i.value;
+    function notString(i) {
+	return typeof i.name !== 'string';
+    }
+
+    function justTheValue(i) {
+	return i.value;
+    }
+    function fixArray(a) {
+	var duplicateKeys = false;
+	function toKeyValue(target, pair) {
+	    duplicateKeys |= (typeof target[pair.name] !== 'undefined');
+	    target[pair.name] = pair.value;
 	}
-
-	function readArray() {
-		d('readArray', input.substring(cursor));
-		var a = [];
-		skipWhitespace();
-		var pair = readPair();
-		while (pair !== null) {
-			a.push(pair);
-			pair = readPair();
-		}
-		if (a.every(notString)) {
-			return a.map(justTheValue);
-		}
-		return a;
+	
+	if (a.every(notString)) { // full array
+	    return a.map(justTheValue);
+	} else if (!a.some(notString)) { // full map
+	    var b = a.reduce({}, toKeyValue);
+	    if (!duplicateKeys) { // duplicate keys
+		return b;
+	    }
 	}
+	return a;
+    }
+
+    function readArray() {
+	d('readArray', input.substring(cursor));
+	var a = [];
+	skipWhitespace();
+	var pair = readPair();
+	while (pair !== null) {
+	    a.push(pair);
+	    pair = readPair();
+	}
+	return a;
+    }
 
 
-	function readQuotedString() {
-		d('readQuotedString', input.substring(cursor));
-		var str = '';
+    function readQuotedString() {
+	d('readQuotedString', input.substring(cursor));
+	var str = '';
 
+	++cursor;
+	var start = cursor;
+	var doubled;
+	do {
+	    while (input.charAt(cursor) !== "'") {
 		++cursor;
-		var start = cursor;
-		var doubled;
-		do {
-			while (input.charAt(cursor) !== "'") {
-				++cursor;
+	    }
+	    str += input.substring(start, cursor);
+	    ++cursor;
+	    doubled = input.charAt(cursor) === "'";
+	    if (doubled) {
+		start = cursor;
+		++cursor;
+	    }
+	} while (doubled);
+	return str;
+    }
+
+    function isSpecial(ch) {
+	return ch === '"' || ch === "'" || ch === '<' || ch === '>';
+    }
+
+    function readUnquotedString() {
+	d('readUnquotedString', input.substring(cursor));
+	var start = cursor;
+	while (cursor < input.length && !isSpecial(input.charAt(cursor))) {
+	    ++cursor;
+	}
+	return input.substring(start, cursor).trimRight();
+    }
+
+    function readString() {
+	var str = '';
+	d('readString', input.substring(cursor));
+	var start;
+	var ch;
+	do {
+	    start = cursor;
+	    skipWhitespace();
+	    ch = input.charAt(cursor); 
+	    if (ch === '\'') {
+		str += readQuotedString();
+	    } else if (ch !== '<' && ch !== '>') {
+		str += readUnquotedString();
+	    }
+	} while(cursor < input.length && cursor > start);
+	return str;
+    }
+
+    function readValue() {
+	d('readValue', input.substring(cursor));
+	skipWhitespace();
+	var value;
+	if (input.charAt(cursor) === '<') {
+	    cursor++;
+	    if (input.charAt(cursor) !== '<') {
+		throw new Error('Arrays need to start with << @' + cursor);
+	    }
+	    cursor++;
+	    value = readArray();	    // read array consumes the trailing '>>'
+	} else {
+	    value = readString();
+	}
+	return value;
+    }
+
+    AWESON.parse = function(str, c) {
+	var value;
+
+	input = str;
+	cursor = c || 0;
+
+	return readValue(true);
+    };
+
+    function isNameOrValue(e) {
+	return e === 'name' || e === 'value';
+    }
+    
+    function isKeyValue(e) {
+	return (typeof e.value !== 'undefined') && Object.keys(e).every(isNameOrValue);
+    }
+
+    AWESON.stringify = function(obj, replacer, sp) {
+	switch (typeof obj) {
+	case 'string':
+	    if (/[<>"]/.test(obj) || obj.charAt(0) === "'") {
+		return "'" + obj.replace(/'/g, "''") + "'";
+	    }
+	    return obj;
+	case 'number':
+	    return obj.toString();
+	case 'object':
+	    if (Array.isArray(obj)) {
+		// special AWESON array
+		if (obj.every(isKeyValue)) {
+		    return '<<' + obj.map(function(e) {
+			var n = '';
+			if (typeof e.name === 'string') {
+			    n += '<' + e.name;
 			}
-			str += input.substring(start, cursor);
-			++cursor;
-			doubled = input.charAt(cursor) === "'";
-			if (doubled) {
-				start = cursor;
-			}
-		} while (doubled);
-		return str;
-	}
-
-	function isSpecial(ch) {
-		return ch === '"' || ch === "'" || ch === '<' || ch === '>';
-	}
-
-	function readUnquotedString() {
-		d('readUnquotedString', input.substring(cursor));
-		var start = cursor;
-		while (cursor < input.length && !isSpecial(input.charAt(cursor))) {
-			++cursor;
+			return n + '>' + e.value;
+		    }).join('') + '>>';
 		}
-		return input.substring(start, cursor).trimRight();
+		// regular array
+		return '<<' + obj.map(function(e) {
+		    return '>' + AWESON.stringify(e);
+		}).join('') + '>>';
+	    }
+	    // flat ol' object
+	    return '<<' + Object.keys(obj).map(function(k) {
+		return '<' + AWESON.stringify(k) + '>' + AWESON.stringify(obj[k]);
+	    }).join('') + '>>';
+	default:
+	    throw new Error('Can\'t stringify that');
 	}
-
-	function readValue() {
-		d('readValue', input.substring(cursor));
-		skipWhitespace();
-		var value;
-		switch (input.charAt(cursor)) {
-			case '<':
-				cursor++;
-				if (input.charAt(cursor) !== '<') {
-					throw new Error('Values can\'t start with just one <');
-				}
-				cursor++;
-				value = readArray();
-				// read array consumes the trailing '>>'
-				break;
-			case '\'':
-				value = readQuotedString();
-				break;
-			default:
-				value = readUnquotedString();
-				break;
-		}
-		return value;
-	}
-
-	AWESON.parse = function(str, c) {
-		var value;
-
-		input = str;
-		cursor = c || 0;
-
-		return readValue();
-	};
-
-	AWESON.stringify = function(obj, replacer, sp) {
-		switch (typeof obj) {
-			case 'string':
-				if (/[<>"]/.test(obj) || obj.charAt(0) === "'") {
-					return "'" + obj.replace(/'/g, "''") + "'";
-				}
-				return obj;
-			case 'number':
-				return obj.toString();
-			case 'object':
-				if (Array.isArray(obj)) {
-					return '<<' + obj.map(function(e) {
-						return '>' + AWESON.stringify(e);
-					}).join('') + '>>';
-				}
-				return '<<' + Object.keys(obj).map(function(k) {
-					return '<' + AWESON.stringify(k) + '>' + AWESON.stringify(obj[k]);
-				}).join('') + '>>';
-			default:
-				throw new Error('Can\'t stringify that');
-		}
-	};
+    };
 }(global || this));
--- js/test.js
+++ js/test.js
@@ -1,20 +1,51 @@
 'use strict';
 require('./aweson.js'); /*global AWESON*/
 
-function test(input) {
-	console.log('INPUT>>', input);
-	var str = AWESON.stringify(input);
-	console.log('  AWESON>>', str);
-	var output = AWESON.parse(str);
-	console.log('  PARSED>>', output);
+var d;
+if (process.env.DEBUG) {
+    d = function() { console.log.apply(console, arguments); };
+} else {
+    d = function() { };
 }
 
-test('Hello World!');
-test('\'With a quote at the start');
-test('Double quotes "');
-test('Angles <>');
-test(123);
-test(1e40);
-test([1,2,3]);
-test({a: 1, b: 2, c: 3});
-test({a: [], b: 1, c: ''});
+
+function stringify(input, output) {
+    d('INPUT>>', input);
+    var str = AWESON.stringify(input);
+    d('  AWESON>>', str);
+    if (str !== output) {
+	throw new Error('Expected:\n' + output + '\nGot:\n' + str);
+    }
+}
+
+stringify('Hello World!', 'Hello World!');
+stringify('\'quote at the start', '\'\'\'quote at the start\'');
+stringify('Double quotes "', '\'Double quotes "\'');
+stringify('Angles <>', '\'Angles <>\'');
+stringify(123, '123');
+stringify(1e40, '1e+40');
+stringify([1, 2, 3], '<<>1>2>3>>');
+stringify({ a: 1, b: 2, c: 3 }, '<<<a>1<b>2<c>3>>');
+stringify({ a: [], b: 1, c: '' }, '<<<a><<>><b>1<c>>>');
+stringify([ { value: 2 }, { name: 'n', value: 3 } ], '<<>2<n>3>>');
+
+function parse(input, output) {
+    d('INPUT>>', input);
+    var parsed = AWESON.parse(input);
+    d('  PARSED>>', parsed);
+    var str = AWESON.stringify(parsed);
+    d('  AWESON>>', str);
+    if (str !== output) {
+	throw new Error('Expected:\n' + output + '\nGot:\n' + str);
+    }
+}
+
+parse('<<>>', '<<>>');
+parse('\'string\'', 'string');
+parse('\'a\'\n\'b\'', 'ab');
+parse('a "comment" b', 'ab');
+parse('a "comment" \' b\' c', 'a bc');
+parse('<<<"c"\'a\'"c">"c"value"c">>', '<<<a>value>>');
+parse('<< >1 >2 >3 >>', '<<>1>2>3>>');
+
+console.log('OK');
